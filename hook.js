5ac2b95a8fb37ba6da02b53511ce3a094e5a56e7
Matt Olson <mattlolson@gmail.com>
2014-10-22 20:20:48 -0700

    Initial commit

---
+++ hook.js
@@ -0,0 +1,17 @@
+// Listen on port 9001
+var gith = require('gith').create( 9001 );
+// Import execFile, to run our bash script
+var execFile = require('child_process').execFile;
+
+gith({
+    repo: 'mlolson/autodeploy'
+}).on( 'push', function( payload ) {
+    if( payload.branch === 'master' )
+    {
+            // Exec a shell script
+            execFile('./hook.sh', function(error, stdout, stderr) {
+                    // Log success in some manner
+                    console.log( 'exec complete' );
+            });
+    }
+});
---
+++ hook.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# First, get the zip file
+wget -O projectmaster.zip -q https://github.com/mlolson/autodeploy/archive/master.zip
+
+# Second, unzip it, if the zip file exists
+if [ -f ./projectmaster.zip ]; then
+    # Unzip the zip file
+    unzip -q ./projectmaster.zip
+
+fi
